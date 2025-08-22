a966bb3ed4566400fd76b10005448d2b7328078c
keremguneyy <121130078+keremguneyy@users.noreply.github.com>
2025-08-22 18:53:50 +0300

    Update data.js

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,15 +1,16 @@
+// Hasta laboratuvar verilerini ve kişisel bilgileri tutan örnek JSON veri
 export const data = [
-{
-  "ast": "",
-  "alt": "",
-  "ggt": "",
-  "alp": "",
-  "totalBilirubin": "",
-  "directBilirubin": "",
-  "albumin": "",
-  "platelet": "",
-  "ldh": "",
-  "isim": "Furkan",
-  "soyisim": "Karan"
-}
+  {
+    "ast": "",
+    "alt": "",
+    "ggt": "",
+    "alp": "",
+    "totalBilirubin": "",
+    "directBilirubin": "",
+    "albumin": "",
+    "platelet": "",
+    "ldh": "",
+    "isim": "Furkan",
+    "soyisim": "Karan"
+  }
 ];
